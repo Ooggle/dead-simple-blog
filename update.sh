710ece8ad500a2dee91f91f3e3436b5e6ec2ec10
Ooggle <33269056+Ooggle@users.noreply.github.com>
2021-07-23 00:33:51 +0200

    :bug: add warning message when directory does not exist

--- update.sh
+++ update.sh
@@ -22,4 +22,6 @@ if [[ -d "${1}" ]]; then
     tar c "$dest_path/" -f "$dest_path/../src_backup/$backup_name"
     (cd "$tmp_path/src/apache2/src/" && tar c .) | (cd "$dest_path" && tar xf -)
     rm -rf "$tmp_path"
+else
+    echo "Directory does not exist or file selected, exiting..."
 fi
